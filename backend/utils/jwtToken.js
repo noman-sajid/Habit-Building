ad42959a91783248afb83ca316c87a819bb4dbb6
noman-sajid <noman15072002@gmail.com>
2025-08-20 17:45:36 +0500

    fixed samesite issue

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict'
+     sameSite: 'None', 
   };
 
   res.status(statusCode).cookie('token', token, options).json({
