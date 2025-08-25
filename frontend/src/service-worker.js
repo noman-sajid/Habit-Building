6040ce6d7d0addd5194b8b4f0514df90099a7767
noman-sajid <noman15072002@gmail.com>
2025-08-25 22:23:49 +0500

    fixed the service worker issue

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,41 +1,8 @@
-// /* eslint-disable no-restricted-globals */
-// import { precacheAndRoute } from 'workbox-precaching';
-// import { registerRoute } from 'workbox-routing';
-// import { NetworkFirst } from 'workbox-strategies';
-
-// // Precache build assets
-// precacheAndRoute(self.__WB_MANIFEST);
-
-// // Offline fallback for navigation requests
-// registerRoute(
-//   ({ request }) => request.mode === 'navigate',
-//   new NetworkFirst({
-//     cacheName: 'pages-cache',
-//     networkTimeoutSeconds: 3,
-//     plugins: [
-//       {
-//         handlerDidError: async () => {
-//           return caches.match('/offline.html');
-//         },
-//       },
-//     ],
-//   })
-// );
-
-// // Cache offline.html on install
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open('offline-cache').then((cache) => {
-//       return cache.add('/offline.html');
-//     })
-//   );
-// });
 
 
 
 
-
-
+/* eslint-env serviceworker */
 /* eslint-disable no-restricted-globals */
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
