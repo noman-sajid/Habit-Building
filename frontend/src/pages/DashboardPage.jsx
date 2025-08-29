9b39f059cae45adc41a7e9144680d0af3d4023ef
noman-sajid <noman15072002@gmail.com>
2025-08-29 12:49:45 +0500

    Clean up

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,49 +1,4 @@
-// // /pages/Dashboard.jsx
-// import React, { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { fetchHabits, fetchHabitSummary } from "../reducers/habitReducer";
 
-// import WelcomeBanner from "../components/dashboard/WelcomeBanner";
-// import WeeklyHabitTracker from "../components/dashboard/WeeklyHabitTracker";
-// import StatsGrid from "../components/dashboard/StatsGrid";
-// import SummaryChart from "../components/dashboard/SummaryChart";
-// import ProgressRing from "../components/dashboard/ProgressRing";
-// import HabitList from "../components/dashboard/HabitList";
-// import QuickActionButton from "../components/dashboard/QuickActionButton";
-
-// const Dashboard = () => {
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchHabits());
-//     dispatch(fetchHabitSummary());
-//   }, [dispatch]);
-
-//   return (
-//     <div className="min-h-screen bg-stone-50 dark:bg-stone-900 text-stone-800 dark:text-stone-200">
-//       <main className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-6 py-6">
-//         <div className="lg:col-span-2 space-y-6">
-//           <WelcomeBanner />
-//           <div className="lg:hidden">
-//             <ProgressRing />
-//           </div>
-//           <HabitList />
-//           <WeeklyHabitTracker />
-//         </div>
-//         <div className="space-y-6">
-//           <div className="hidden lg:block">
-//             <ProgressRing />
-//           </div>
-//           <SummaryChart />
-//           <StatsGrid />
-//         </div>
-//       </main>
-//       <QuickActionButton />
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
 
 
 
@@ -61,6 +16,7 @@ import SummaryChart from "../components/dashboard/SummaryChart";
 import ProgressRing from "../components/dashboard/ProgressRing";
 import HabitList from "../components/dashboard/HabitList";
 import QuickActionButton from "../components/dashboard/QuickActionButton";
+
 
 const Dashboard = () => {
   const dispatch = useDispatch();
